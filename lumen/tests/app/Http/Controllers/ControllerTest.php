9dfae0512d677e6008386fa1794923615c6c2682
luis <luispaulolpsn@gmail.com>
2022-03-01 01:42:25 -0300

    colocando espaco

--- lumen/tests/app/Http/Controllers/ControllerTest.php
+++ lumen/tests/app/Http/Controllers/ControllerTest.php
@@ -14,6 +14,7 @@ class ControllerTest extends TestCase
     public function testController()
     {
         $response = $this->call('GET', '/api/list/');
+
         $this->assertEquals(200, $response->status());
     }
 }
